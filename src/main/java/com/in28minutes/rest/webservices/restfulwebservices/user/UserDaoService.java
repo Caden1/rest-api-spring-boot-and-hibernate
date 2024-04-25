eeb2042f9ef485da007f9ec6400a08b21f3af1f8
Caden Barton <cadenbarton@Cadens-MacBook-Air.local>
2024-04-25 11:38:49 -0600

    Implemented user creation

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -12,10 +12,12 @@ public class UserDaoService {
 	
 	private static List<User> users = new ArrayList<User>();
 	
+	private static int usersCount = 0;
+	
 	static {
-		users.add(new User(1, "John", LocalDate.now().minusYears(30)));
-		users.add(new User(2, "Jane", LocalDate.now().minusYears(25)));
-		users.add(new User(3, "Caden", LocalDate.now().minusYears(20)));
+		users.add(new User(++usersCount, "John", LocalDate.now().minusYears(30)));
+		users.add(new User(++usersCount, "Jane", LocalDate.now().minusYears(25)));
+		users.add(new User(++usersCount, "Caden", LocalDate.now().minusYears(20)));
 	}
 	
 	public List<User> findAll() {
@@ -25,6 +27,12 @@ public class UserDaoService {
 	public User findOne(int id) {
 		Predicate<? super User> predicate = user -> user.getId().equals(id);
 		return users.stream().filter(predicate).findFirst().get();
+	}
+	
+	public User save(User user) {
+		user.setId(++usersCount);
+		users.add(user);
+		return user;
 	}
 
 }
