c5a1869ecc0c0381734e423aa7802d6bff3a7d15
Caden Barton <cadenbarton@Cadens-MacBook-Air.local>
2024-05-04 14:22:46 -0600

    Added connection to H2 database and added user_details table

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into user_details(birth_date, id, name)
+values(current_date(), 1001, 'Caden');
+
+insert into user_details(birth_date, id, name)
+values(current_date(), 1002, 'John');
+
+insert into user_details(birth_date, id, name)
+values(current_date(), 1003, 'Jane');
