368110cbe72c546de930f3c07c2b45fae84a9308
Caden Barton <cadenbarton@Cadens-MacBook-Air.local>
2024-05-04 15:02:05 -0600

    Added JPA to allow easier access to H2 database

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -18,12 +18,14 @@ public class User {
 	private Integer id;
 	
 	@Size(min=2, message="Name should have atleast 2 characters")
-	@JsonProperty("user_name")
+	//@JsonProperty("user_name")
 	private String name;
 	
 	@Past(message="Birth Date should be in the past")
-	@JsonProperty("birth_date")
+	//@JsonProperty("birth_date")
 	private LocalDate birthDate;
+	
+	protected User() { }
 	
 	public User(Integer id, String name, LocalDate birthDate) {
 		super();
