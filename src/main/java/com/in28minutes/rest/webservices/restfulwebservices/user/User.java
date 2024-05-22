b41f703399a24f861eea83af0fde776471908961
Caden Barton <cadenbarton@Cadens-MacBook-Air.local>
2024-05-22 11:08:32 -0600

    Added OneToMany and ManyToOne relationships for posts and user. Added GET API to get posts for a user

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -1,12 +1,14 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
 import java.time.LocalDate;
+import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -18,12 +20,14 @@ public class User {
 	private Integer id;
 	
 	@Size(min=2, message="Name should have atleast 2 characters")
-	//@JsonProperty("user_name")
 	private String name;
 	
 	@Past(message="Birth Date should be in the past")
-	//@JsonProperty("birth_date")
 	private LocalDate birthDate;
+	
+	@OneToMany(mappedBy = "user")
+	@JsonIgnore
+	private List<Post> posts;
 	
 	protected User() { }
 	
@@ -37,20 +41,33 @@ public class User {
 	public Integer getId() {
 		return id;
 	}
+	
 	public void setId(Integer id) {
 		this.id = id;
 	}
+	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public LocalDate getBirthDate() {
 		return birthDate;
 	}
+	
 	public void setBirthDate(LocalDate birthDate) {
 		this.birthDate = birthDate;
+	}
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	@Override
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,15 @@ values(current_date(), 1002, 'John');
 
 insert into user_details(birth_date, id, name)
 values(current_date(), 1003, 'Jane');
+
+insert into post(id, user_id, description)
+values(2001, 1001, 'I want to learn AWS');
+
+insert into post(id, user_id, description)
+values(2002, 1001, 'I want to learn DevOps');
+
+insert into post(id, user_id, description)
+values(2003, 1002, 'I want to get AWS certified');
+
+insert into post(id, user_id, description)
+values(2004, 1002, 'I want to learn Multi Cloud');
